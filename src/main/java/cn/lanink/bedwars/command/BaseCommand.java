77863dc43a06715804a951730b30c15e8070acc7
lt_name <572413378@qq.com>
2021-01-31 12:56:52 +0800

    Stuff

---
+++ src/main/java/cn/lanink/bedwars/command/BaseCommand.java
@@ -0,0 +1,96 @@
+package cn.lanink.bedwars.command;
+
+import cn.lanink.bedwars.BedWars;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author SmallasWater
+ */
+public abstract class BaseCommand extends Command {
+
+    private final ArrayList<BaseSubCommand> subCommand = new ArrayList<>();
+    private final ConcurrentHashMap<String, Integer> subCommands = new ConcurrentHashMap<>();
+    protected BedWars bedWars = BedWars.getInstance();
+
+    public BaseCommand(String name, String description) {
+        super(name,description);
+    }
+
+    /**
+     * 判断权限
+     * @param sender 玩家
+     * @return 是否拥有权限
+     */
+    public boolean hasPermission(CommandSender sender) {
+        return sender.hasPermission(this.getPermission());
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String s, String[] args) {
+        if(hasPermission(sender)) {
+            if(args.length > 0) {
+                String subCommand = args[0].toLowerCase();
+                if (subCommands.containsKey(subCommand)) {
+                    BaseSubCommand command = this.subCommand.get(this.subCommands.get(subCommand));
+                    if (command.canUser(sender)) {
+                        return command.execute(sender, s, args);
+                    }else if (sender.isPlayer()) {
+                        sender.sendMessage(this.bedWars.getLanguage(sender).translateString("cmd_noPermission"));
+                    }else {
+                        sender.sendMessage(this.bedWars.getLanguage(sender).translateString("cmd_cannotUsedInConsole"));
+                    }
+                }else {
+                    this.sendHelp(sender);
+                }
+            }else {
+                if (sender.isPlayer()) {
+                    this.sendUi(sender);
+                }else {
+                    this.sendHelp(sender);
+                }
+            }
+            return true;
+        }
+        sender.sendMessage(this.bedWars.getLanguage(sender).translateString("cmd_noPermission"));
+        return true;
+    }
+
+    /**
+     * 发送帮助
+     * @param sender 玩家
+     * */
+    public abstract void sendHelp(CommandSender sender);
+
+    /**
+     * 发送UI
+     * @param sender 玩家
+     */
+    public abstract void sendUi(CommandSender sender);
+
+    protected void addSubCommand(BaseSubCommand cmd) {
+        this.subCommand.add(cmd);
+        int commandId = (this.subCommand.size()) - 1;
+        this.subCommands.put(cmd.getName().toLowerCase(), commandId);
+        for (String alias : cmd.getAliases()) {
+            this.subCommands.put(alias.toLowerCase(), commandId);
+        }
+    }
+
+    protected void loadCommandBase(){
+        this.commandParameters.clear();
+        for(BaseSubCommand subCommand : this.subCommand) {
+            LinkedList<CommandParameter> parameters = new LinkedList<>();
+            parameters.add(CommandParameter.newEnum(subCommand.getName(), new String[]{subCommand.getName()}));
+            parameters.addAll(Arrays.asList(subCommand.getParameters()));
+            this.commandParameters.put(subCommand.getName(), parameters.toArray(new CommandParameter[0]));
+        }
+    }
+
+}
---
+++ src/main/java/cn/lanink/bedwars/command/BaseSubCommand.java
@@ -0,0 +1,59 @@
+package cn.lanink.bedwars.command;
+
+import cn.lanink.bedwars.BedWars;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
+
+
+/**
+ * @author SmallasWater
+ */
+public abstract class BaseSubCommand {
+
+    protected BedWars bedWars = BedWars.getInstance();
+    private final String name;
+
+    protected BaseSubCommand(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param sender CommandSender
+     * @return boolean
+     */
+    public abstract boolean canUser(CommandSender sender);
+
+    public String getDescription(){
+        return "";
+    }
+
+    /**
+     * 获取名称
+     * @return string
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * 获取别名
+     * @return string[]
+     */
+    public abstract String[] getAliases();
+
+    /**
+     * 命令响应
+     * @param sender the sender      - CommandSender
+     * @param args   The arrugements      - String[]
+     * @param label  label..
+     * @return true if true
+     */
+    public abstract boolean execute(CommandSender sender, String label, String[] args);
+
+    /**
+     * 指令参数.
+     * @return  提示参数
+     * */
+    public abstract CommandParameter[] getParameters();
+
+}
