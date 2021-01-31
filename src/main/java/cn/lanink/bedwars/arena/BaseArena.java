9a8c59111218b15ad7c7dd45101afbe45cdf0eb4
lt_name <572413378@qq.com>
2021-01-31 13:32:15 +0800

    添加ArenaLoadException

--- src/main/java/cn/lanink/bedwars/arena/BaseArena.java
+++ src/main/java/cn/lanink/bedwars/arena/BaseArena.java
@@ -1,6 +1,9 @@
 package cn.lanink.bedwars.arena;
 
+import cn.lanink.bedwars.utils.exception.ArenaLoadException;
 import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.level.Level;
 import cn.nukkit.utils.Config;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,11 +16,18 @@ public class BaseArena extends ArenaConfig {
 
     private ArenaStatus arenaStatus;
 
+    private Level gameWorld;
+
     private final ConcurrentHashMap<Player, Team> players = new ConcurrentHashMap<>();
 
-    public BaseArena(@NotNull Config config) {
+    public BaseArena(@NotNull Config config) throws ArenaLoadException {
         super(config);
         //TODO Init Arena
+        if (!Server.getInstance().loadLevel(this.getGameWorldName())) {
+            throw new ArenaLoadException("世界: " + this.getGameWorldName() + " 加载失败！");
+        }
+        this.gameWorld = Server.getInstance().getLevelByName(this.getGameWorldName());
+
     }
 
     public boolean joinRoom(@NotNull Player player) {
---
+++ src/main/java/cn/lanink/bedwars/utils/exception/ArenaLoadException.java
@@ -0,0 +1,16 @@
+package cn.lanink.bedwars.utils.exception;
+
+/**
+ * @author lt_name
+ */
+public class ArenaLoadException extends Exception {
+
+    public ArenaLoadException() {
+        super();
+    }
+
+    public ArenaLoadException(String message) {
+        super(message);
+    }
+
+}
