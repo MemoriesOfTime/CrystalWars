6acffc9e49b5897f2e704aeab7a56bd7065f61e1
lt_name <572413378@qq.com>
2021-02-04 18:36:45 +0800

    Stuff

--- src/main/java/cn/lanink/bedwars/items/BaseItem.java
+++ src/main/java/cn/lanink/bedwars/items/BaseItem.java
@@ -1,12 +1,14 @@
 package cn.lanink.bedwars.items;
 
 import cn.nukkit.item.Item;
+import lombok.Getter;
 
 /**
  * @author lt_name
  */
 public class BaseItem {
 
+    @Getter
     private Item item;
 
     public BaseItem() {
