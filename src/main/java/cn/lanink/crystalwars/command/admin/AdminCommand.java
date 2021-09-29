39a30631821c8098cd06a06af06a203dadabd390
LT_Name <572413378@qq.com>
2021-09-29 12:04:37 +0800

    添加StartGame命令

--- src/main/java/cn/lanink/crystalwars/command/admin/AdminCommand.java
+++ src/main/java/cn/lanink/crystalwars/command/admin/AdminCommand.java
@@ -3,6 +3,7 @@ package cn.lanink.crystalwars.command.admin;
 import cn.lanink.crystalwars.command.BaseCommand;
 import cn.lanink.crystalwars.command.admin.sub.CreateArena;
 import cn.lanink.crystalwars.command.admin.sub.Reload;
+import cn.lanink.crystalwars.command.admin.sub.StartGame;
 import cn.lanink.crystalwars.command.admin.sub.UnloadArena;
 import cn.lanink.crystalwars.form.FormHelper;
 import cn.nukkit.Player;
@@ -21,6 +22,8 @@ public class AdminCommand extends BaseCommand {
         //TODO
         this.addSubCommand(new CreateArena("CreateArena"));
 
+        this.addSubCommand(new StartGame("StartGame"));
+
         this.addSubCommand(new UnloadArena("UnloadArena"));
         this.addSubCommand(new Reload("reload"));
 
@@ -31,6 +34,7 @@ public class AdminCommand extends BaseCommand {
         sender.sendMessage(
                 "§a/" + this.getName() + " §e打开GUI\n" +
                         "§a/" + this.getName() + " CreateArena <地图名称> §e创建新的游戏房间\n" +
+                        "§a/" + this.getName() + " StartGame [地图名称] §e跳过等待倒计时，开始游戏\n" +
                         "§a/" + this.getName() + " UnloadArena <游戏房间名称> §e卸载指定游戏房间\n" +
                         "§a/" + this.getName() + " UnloadArena §e卸载所有游戏房间\n" +
                         "§a/" + this.getName() + " reload §e重载插件配置\n"
