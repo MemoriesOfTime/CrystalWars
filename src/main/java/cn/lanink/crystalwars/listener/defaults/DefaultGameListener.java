c970012219c10fdf875aed0b5918bc50a1ba828f
LT_Name <572413378@qq.com>
2022-03-25 21:10:11 +0800

    禁止直接脱下护甲

--- src/main/java/cn/lanink/crystalwars/listener/defaults/DefaultGameListener.java
+++ src/main/java/cn/lanink/crystalwars/listener/defaults/DefaultGameListener.java
@@ -16,6 +16,7 @@ import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.event.entity.EntityExplodeEvent;
 import cn.nukkit.event.inventory.CraftItemEvent;
+import cn.nukkit.event.inventory.InventoryClickEvent;
 import cn.nukkit.event.player.PlayerFoodLevelChangeEvent;
 import cn.nukkit.event.player.PlayerGameModeChangeEvent;
 import cn.nukkit.event.player.PlayerItemHeldEvent;
@@ -224,6 +225,33 @@ public class DefaultGameListener extends BaseGameListener<BaseArena> {
         Level level = event.getPlayer() == null ? null : event.getPlayer().getLevel();
         if (level != null && this.getListenerRooms().containsKey(level.getFolderName())) {
             event.setCancelled(false);
+        }
+    }
+
+    /**
+     * 玩家点击背包栏格子事件
+     * @param event 事件
+     */
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = event.getPlayer();
+        if (player == null || event.getInventory() == null) {
+            return;
+        }
+        BaseArena arena = this.getListenerRoom(player.getLevel());
+        if (arena == null || !arena.isPlaying(player)) {
+            return;
+        }
+        Item sourceItem = event.getSourceItem();
+        //TODO 全使用NBT判断
+        if (sourceItem.isArmor() || (sourceItem.hasCompoundTag() && sourceItem.getNamedTag().getBoolean("cannotTakeItOff")) &&
+                event.getHeldItem().getId() == 0) {
+            event.setCancelled(true);
+            return;
+        }
+        if ((sourceItem.hasCompoundTag() && sourceItem.getNamedTag().getBoolean("cannotClickOnInventory")) ||
+                (event.getHeldItem().hasCompoundTag() && event.getHeldItem().getNamedTag().getBoolean("cannotClickOnInventory"))) {
+            event.setCancelled(true);
         }
     }
 
